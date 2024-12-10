b8ec6ff03ed157da9f8657129828d7d8f2b14667
shirelsallary <shirel4239@gmail.com>
2024-12-10 20:03:48 +0200

    changes in isNumber,equals,numSwitch, baseSwitch

--- src/Ex1.java
+++ src/Ex1.java
@@ -20,12 +20,15 @@ public class Ex1
      */
     public static int number2Int(String num)
     {
-        if(isNumber(num)==true)
+        if(isNumber(num))
         {
              String[] arr=num.split("b") ;
                 if (arr.length==1)
-                    return Integer.parseInt(num);
-            return (Integer.parseInt(arr[0],Integer.parseInt(arr[1])));      //return the number as his decimal representation
+                    return Integer.parseInt(num);//10 base
+
+            int base=baseSwitch(arr[0]);
+            int n=numSwitch(arr[1]);
+            return Integer.parseInt(String.valueOf(n),base);
         }
         return -1;
     }
@@ -41,54 +44,41 @@ public class Ex1
 
         if (!a.contains("b"))   //10 base
         {
-            return true;
-        } else if (a.charAt(a.length() - 2) == 'b') //if the b is in the right place
+            if(checkDigit(a)==true)
+                return true;
+            else return false;
+
+        }
+        if (a.charAt(a.length() - 2) == 'b') //if the b is in the right place
         {
             String[] nums = a.split("b");
-            if (nums.length < 3)// if there are values on both sides of b
+
+            if (nums.length !=2)// if there are values on both sides of b
             {
                 return false; // if not
             }
-            if (Integer.parseInt(nums[1]) < 0) {
+
+            int base2;
+            int num2;
+            if(!checkDigit(nums[1]))
+                base2=baseSwitch(nums[1]);
+            else
+                base2=Integer.parseInt(nums[1]);
+
+            if((base2==-1))
                 return false;
-            }
-            if (Integer.parseInt(nums[1]) < 10)// if the base is number
+
+            for(int i=0;i<nums[0].length();i++)                               //check if the number is posibble depending on the base
             {
-                if (checkDigit(nums[1])==true)
-                {
-                    int base1=Integer.parseInt(nums[0]);
-                    for(int i=0;i<nums[0].length();i++)                               //check if the number is posibble depending on the base
-                    {
-                        if (nums[0].charAt(i)>=base1)                                   //if one of them is bigger/equals-not good
-                            return false;
-                    }
-                    return true;
-                }
+                num2=(numSwitch(nums[0].charAt(i)));
+                if (num2==-1)
+                    return false;
+                if(num2>=base2)
+                    return false;                                  //if one of them is bigger/equals-not good
             }
-
-                if ((toBase(nums[1]) < 17)&&(toBase(nums[1]) >10))
-                {
-                    if (checkDigit(String.valueOf(toBase(nums[1]))))//check the base if it a char
-                    {
-                        int base1=Integer.parseInt(nums[0]);
-                        for(int i=0;i<nums[0].length();i++)                               //check if the number is posibble depending on the base
-                        {
-                            if(checkDigit(String.valueOf(nums[0].charAt(i)))) {
-                                if (nums[0].charAt(i) >= base1)                                   //if one of them is bigger/equals-not good
-                                    return false;
-                            }
-                            int currentNum=toBase(String.valueOf(nums[0].charAt(i)));
-                            if((currentNum<17)&&(currentNum>10)) {
-                                if(currentNum>base1)
-                                    return false;
-                                return true;
-                            }
-                        }
-
-                    }
-                }
+            return true;
         }
-        return true;
+        return false;
     }
 
             /**
@@ -119,12 +109,10 @@ public class Ex1
              * @return true iff the two numbers have the same values.
              */
             public static boolean equals (String n1, String n2){
-            if (n1.equals(n2))
-                return true;                                    //if they are equals
-            return false;
-        }
+            return number2Int(n1)==number2Int(n2);
+            }
 
-            /**
+         /*   /**
              * This static function search for the array index with the largest number (in value).
              * In case there are more than one maximum - returns the first index.
              * Note: you can assume that the array is not null and is not empty, yet it may contain null or none-valid numbers (with value -1).
@@ -132,26 +120,68 @@ public class Ex1
              * @return the index in the array in with the largest number (in value).
              *
              */
-            public static int maxIndex (String[]arr){
 
+
+
+
+
+    public static boolean checkDigit (String n) {
+        for (int i = 0; i < n.length(); i++) {
+            if (!Character.isDigit(n.charAt(i)))
+                return false;
+        }
+        return true;
+    }
+
+
+    public static int numSwitch(char num) {
+        int n = -1;
+        if(Character.isDigit(num)) {
+            if (num == '1') return 1;
+            if (num == '2') return 2;
+            if (num == '3') return 3;
+            if (num == '4') return 4;
+            if (num == '5') return 5;
+            if (num == '6') return 6;
+            if (num == '7') return 7;
+            if (num == '8') return 8;
+            if (num == '9') return 9;
 
         }
+        if(num=='A') return 10;
+        if(num=='B') return 11;
+        if(num=='C') return 12;
+        if(num=='D') return 13;
+        if(num=='E') return 14;
+        if(num=='F') return 15;
+
+        return n;
+    }
+
+    public static int baseSwitch(String num){
+        int n = -1;
+        if(num.equals("2")) return 2;
+        if(num.equals("3")) return 2;
+        if(num.equals("4")) return 2;
+        if(num.equals("5")) return 2;
+        if(num.equals("6")) return 2;
+        if(num.equals("7")) return 2;
+        if(num.equals("8")) return 2;
+        if(num.equals("9")) return 2;
+        if(num.equals("A")) return 10;
+        if(num.equals("B")) return 10;
+        if(num.equals("C")) return 10;
+        if(num.equals("D")) return 10;
+        if(num.equals("E")) return 10;
+        if(num.equals("F")) return 10;
+        if(num.equals("G")) return 10;
+
+        return n;
+
+    }
 
 
-            public static int toBase (String num){
-            return (Integer.parseInt(num) - 54);
-        }
-
-            public static boolean checkDigit (String n){
-            for (int i = 0; i < n.length(); i++) {
-                if (!Character.isDigit(n.charAt(i)))
-                    return false;
-            }
-            return true;
-        }
-
-
-        }
+}
 
 
 
